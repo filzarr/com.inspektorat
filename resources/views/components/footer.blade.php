da34aa911328e51a58b2ec896557aa0855ed0fd1
filzarr <filzaramadhan2003@students.usu.ac.id>
2024-07-18 15:35:13 +0700

    fix: perbaikan footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,96 +1,59 @@
-{{-- <div class="mt-16 w-full px-28 bg-[#3E6E8B] py-10">
-    <div class="grid grid-cols-5 gap-20">
-        <div class="">
-            <div class="flex justify-start">
-                <p class="font-bold text-2xl text-white/80 ">Alamat</p>
-            </div>
+ 
 
-            <p class="font-bold mt-5 text-white/80 ">Jl. K.H. Wahid Hasyim No.8, Merdeka, Kec. Medan Baru, Kota
-                Medan, Sumatera Utara 20222</p>
-
+<footer class="px-[8px] py-[32px] sm:px-[16px]  md:px-[32px] lg:px-[64px] xl:px-[128px] space-y-[24px] bg-[#2980B9]">
+    <header class="flex gap-2">
+        <img src="https://inspektorat.sumutprov.go.id/logo_inspektorat.png" class="h-[30px] w-[30px] lg:h-[50px] lg:w-[50px]"
+            alt="Logo Inspektorat Provinsi Sumatera Utara">
+        <div class=" space-y-0">
+            <p class=" font-bold text-[#ffffff] text-sm lg:text-base">Inspektorat <br> Provinsi Sumatera Utara</p> 
         </div>
-        <div class="">
-            <div class="flex justify-start">
-                <p class="font-bold text-2xl text-white/80 ">Program</p>
-            </div>
-            <ul class="font-bold mt-5 text-white/80 ">
-                <li>Saber Pungli</li>
-                <li>PPID</li>
+    </header>
+    <div class="  xl:mt-0 xl:flex justify-between items-start gap-8">
+        <ul class=" flex flex-col gap-8">
+            <li class="flex gap-2  ">
+                <iconify-icon icon="lucide:map-pin" class=" text-white" height="32" width="32"></iconify-icon>
+                <div class=" space-y-2">
+                    <p class="  font-bold text-white ">Lokasi</p>
+                    <p class=" font-medium text-white text-sm">Jl. K.H. Wahid Hasyim No.8, Merdeka, Kec. Medan Baru, <br> Kota Medan, Sumatera Utara 20222</p>
+                </div>
+            </li>
+            <li class="flex gap-2 items-center">
+                <iconify-icon icon="entypo:old-phone" class=" text-white" height="32" width="32"></iconify-icon>
+                <p class="  font-bold text-white ">-</p> 
+            </li>
+            <li class="flex gap-2 items-center">
+                <iconify-icon icon="ic:baseline-email" class=" text-white" height="32" width="32"></iconify-icon>
+                <p class="  font-medium text-white text-sm ">Provsuinspektorat@gmail.com</p> 
+            </li>
+        </ul>
+        <div class=" my-6 grid grid-cols-2 xl:flex gap-8">
+            <ul class=" space-y-4">
+                <li class="font-reguler text-sm text-white">Hak Cipta Website</li>
+                <li class="font-reguler text-sm text-white">PPID</li>
+                <li class="font-reguler text-sm text-white">Saber Pungli</li>
             </ul>
-
+            <ul class=" space-y-4">
+                <li class="font-reguler text-sm text-white">Info Sumut</li>
+                <li class="font-reguler text-sm text-white">Span Lapor</li>
+                <li class="font-reguler text-sm text-white">Saber Pungli</li>
+            </ul>
+            <ul class=" space-y-4">
+                <li class="font-reguler text-sm text-white">Peta Situs</li>
+                <li class="font-reguler text-sm text-white">Aturan Penggunaan</li>
+                <li class="font-reguler text-sm text-white">Berita</li>
+            </ul>
         </div>
-        <div class="">
-            <div class="flex justify-start">
-                <p class="font-bold text-2xl text-white/80 ">Visi Misi</p>
-
-
-            </div>
+        <div class=" rounded-md overflow-hidden h-[300px] col-span-2 w-full xl:w-[500px]">
+            <iframe class=" w-full h-full" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3982.057349129756!2d98.65560627488736!3d3.5742878963998845!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30312fd668f63c3f%3A0x3838ce024c2994ea!2sInspektorat%20Provinsi%20Sumatera%20Utara!5e0!3m2!1sid!2sid!4v1686853138098!5m2!1sid!2sid" frameborder="0"></iframe>
         </div>
-        <div class=" col-span-2 w-full h-96"><iframe class=" w-full min-h-full"
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3982.057349129756!2d98.65560627488736!3d3.5742878963998845!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30312fd668f63c3f%3A0x3838ce024c2994ea!2sInspektorat%20Provinsi%20Sumatera%20Utara!5e0!3m2!1sid!2sid!4v1686853138098!5m2!1sid!2sid"
-                style="border:0;" allowfullscreen="" loading="lazy"
-                referrerpolicy="no-referrer-when-downgrade"></iframe></div>
-
-
     </div>
-</div> --}}
-
-<footer class="bg-[#3E6E8B]">
-    <div class="mx-auto w-full max-w-screen-xl">
-        <div class="grid grid-cols-2 gap-8 px-4 py-6 lg:py-8 md:grid-cols-4">
-            <div>
-                <h2 class="mb-6 text-sm font-semibold text-gray-100 uppercase dark:text-white">Alamat</h2>
-                <ul class="text-gray-100 dark:text-gray-100 font-medium">
-                    <li class="mb-4">
-                        <a href="#" class=" hover:underline">Jl. K.H. Wahid Hasyim No.8, Merdeka, Kec. Medan Baru, Kota
-                            Medan, Sumatera Utara 20222</a>
-                    </li>
-                    
-                </ul>
-            </div>
-            <div>
-                <h2 class="mb-6 text-sm font-semibold text-gray-100 uppercase dark:text-white">Program</h2>
-                <ul class="text-gray-100 dark:text-gray-100 font-medium">
-                    <li class="mb-4">
-                        <a href="#" class="hover:underline">Saber Pungli</a>
-                    </li>
-                    <li class="mb-4">
-                        <a href="#" class="hover:underline">PPID</a>
-                    </li>
-                
-                </ul>
-            </div>
-            <div>
-                <h2 class="mb-6 text-sm font-semibold text-gray-100 uppercase dark:text-white">Visi Misi</h2>
-                <ul class="text-gray-100 dark:text-gray-100 font-medium">
-                    <li class="mb-4">
-                        <a href="#" class="hover:underline">Privacy Policy</a>
-                    </li>
-             
-                </ul>
-            </div>
-            <div>
-                <h2 class="mb-6 text-sm font-semibold text-gray-100 uppercase dark:text-white">Menu</h2>
-                <ul class="text-gray-100 dark:text-gray-100 font-medium">
-                    <li class="mb-4">
-                        <a href="#" class="hover:underline">PPID</a>
-                    </li>
-                    <li class="mb-4">
-                        <a href="#" class="hover:underline">Irban</a>
-                    </li>
-                    <li class="mb-4">
-                        <a href="#" class="hover:underline">Regulasi</a>
-                    </li>
-                    <li class="mb-4">
-                        <a href="#" class="hover:underline">Profile</a>
-                    </li>
-                </ul>
-            </div>
-            <div class=" col-span-2 w-full h-96"><iframe class=" w-full min-h-full"
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3982.057349129756!2d98.65560627488736!3d3.5742878963998845!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30312fd668f63c3f%3A0x3838ce024c2994ea!2sInspektorat%20Provinsi%20Sumatera%20Utara!5e0!3m2!1sid!2sid!4v1686853138098!5m2!1sid!2sid"
-                style="border:0;" allowfullscreen="" loading="lazy"
-                referrerpolicy="no-referrer-when-downgrade"></iframe></div>
+    <div class=" py-[24px] border-t-2 border-[#ffff] space-y-3">
+        <div class=" gap-2 flex justify-center text-center">
+            <iconify-icon icon="iconoir:copyright" class=" text-white" height="24" width="24"></iconify-icon>
+            <p class="font-bold text-sm text-white">Copyright Inspektorat Provinsi Sumatera Utara</p>
         </div>
-       
+        <div class=" gap-2 flex justify-center text-center"> 
+            <p class="font-medium underline text-sm text-white">Term Of Use / Privacy and policy</p>
+        </div>
     </div>
 </footer>
