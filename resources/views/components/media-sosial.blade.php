dd95fbd2850e070324949be8b99c9427075af3c2
filzarr <filzaramadhan2003@students.usu.ac.id>
2024-08-25 11:11:57 +0700

    fix: perbaikan responsive medsos

--- resources/views/components/media-sosial.blade.php
+++ resources/views/components/media-sosial.blade.php
@@ -1,19 +1,47 @@
 <div class=" fixed left-0 top-[40%] w-fit z-50">
     <div class="flex flex-col gap-0">
-        <a href="https://www.facebook.com/profile.php?id=61561768439784" target="_blank" class="w-fit max-xl:px-2 px-1 pt-1 max-xl:pt-2 group hover:bg-gray-600 transition-all duration-100 ease-in bg-[#4080FF]">
-            <iconify-icon class=" max-xl:text-2xl text-white text-base " icon="mdi:facebook"></iconify-icon>
+        <a href="https://www.facebook.com/profile.php?id=61561768439784" target="_blank"
+            class="   medsos-container group hover:bg-gray-600 transition-all duration-100 ease-in bg-[#4080FF]">
+            <iconify-icon class=" medsos-item   text-white  " icon="mdi:facebook"></iconify-icon>
         </a>
-        <a href="https://www.instagram.com/official_inspektoratprovsu?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==" target="_blank" class="w-fit max-xl:px-2 px-1 pt-1 max-xl:pt-2 hover:bg-gray-600 transition-all duration-100 ease-in bg-[#c22626]">
-            <iconify-icon class="max-xl:text-2xl text-white text-base" icon="mdi:instagram"></iconify-icon>
+        <a href="https://www.instagram.com/official_inspektoratprovsu?utm_source=ig_web_button_share_sheet&amp;igsh=ZDNlZDc0MzIxNw=="
+            target="_blank"
+            class="medsos-container group hover:bg-gray-600 transition-all duration-100 ease-in bg-[#c22626]">
+            <iconify-icon class="medsos-item   text-white" icon="mdi:instagram"></iconify-icon>
         </a>
-        <a href="" target="_blank" class="w-fit max-xl:px-2 px-1 pt-1 max-xl:pt-2 hover:bg-gray-600 transition-all duration-100 ease-in bg-[#ef4e41]">
-            <iconify-icon class="max-xl:text-2xl text-white text-base" icon="mdi:youtube"></iconify-icon>
+        <a href="" target="_blank" class="medsos-container group hover:bg-gray-600 transition-all duration-100 ease-in bg-[#ef4e41]">
+            <iconify-icon class="medsos-item   text-white" icon="mdi:youtube"></iconify-icon>
         </a>
-        <a href="https://x.com/Itprovsu" target="_blank" class="w-fit max-xl:px-2 px-1 pt-1 max-xl:pt-2 hover:bg-gray-600 transition-all duration-100 ease-in bg-[#40bff5]">
-            <iconify-icon class="max-xl:text-2xl text-white text-base" icon="mdi:twitter"></iconify-icon>
+        <a href="https://x.com/Itprovsu" target="_blank" class="medsos-container group hover:bg-gray-600 transition-all duration-100 ease-in bg-[#40bff5]">
+            <iconify-icon class="medsos-item   text-white" icon="mdi:twitter"></iconify-icon>
         </a>
-        <a href="https://www.tiktok.com/@inspektorat.sumut?_r=1&_d=ebclmie87id827&sec_uid=MS4wLjABAAAAULt8WojzS21j6aNqUqa1zLrWNvFav8QAcY03uN5hqQISurLpgKR5Oz7F4pilNaE8&share_author_id=7386504342726476806&sharer_language=en&source=h5_t&u_code=ef17i8jceie060&ug_btm=b8727,b0&social_share_type=4&utm_source=copy&sec_user_id=MS4wLjABAAAAULt8WojzS21j6aNqUqa1zLrWNvFav8QAcY03uN5hqQISurLpgKR5Oz7F4pilNaE8&tt_from=copy&utm_medium=ios&utm_campaign=client_share&enable_checksum=1&user_id=7386504342726476806&share_link_id=A2310158-4DEA-4E6B-BF05-779785ED0917&share_app_id=1180" class="w-fit max-xl:px-2 px-1 pt-1 max-xl:pt-2 hover:bg-gray-600 transition-all duration-100 ease-in bg-[#000000]" target="_blank">
-            <iconify-icon class="max-xl:text-2xl text-white text-base" icon="ic:baseline-tiktok"></iconify-icon>
+        <a href="https://www.tiktok.com/@inspektorat.sumut?_r=1&amp;_d=ebclmie87id827&amp;sec_uid=MS4wLjABAAAAULt8WojzS21j6aNqUqa1zLrWNvFav8QAcY03uN5hqQISurLpgKR5Oz7F4pilNaE8&amp;share_author_id=7386504342726476806&amp;sharer_language=en&amp;source=h5_t&amp;u_code=ef17i8jceie060&amp;ug_btm=b8727,b0&amp;social_share_type=4&amp;utm_source=copy&amp;sec_user_id=MS4wLjABAAAAULt8WojzS21j6aNqUqa1zLrWNvFav8QAcY03uN5hqQISurLpgKR5Oz7F4pilNaE8&amp;tt_from=copy&amp;utm_medium=ios&amp;utm_campaign=client_share&amp;enable_checksum=1&amp;user_id=7386504342726476806&amp;share_link_id=A2310158-4DEA-4E6B-BF05-779785ED0917&amp;share_app_id=1180" class="medsos-container group hover:bg-gray-600 transition-all duration-100 ease-in bg-[#000000]" target="_blank">
+            <iconify-icon class="medsos-item   text-white" icon="ic:baseline-tiktok"></iconify-icon>
         </a>
     </div>
 </div>
+
+<style>
+    .medsos-container {
+        padding-top: 8px;
+        padding-left: 8px;
+        padding-right: 8px;
+    }
+
+    .medsos-item {
+        font-size: 24px
+    }
+
+    @media screen and (max-width:1200px) {
+        .medsos-container {
+            padding-top: 4px;
+            padding-left: 4px;
+            padding-right: 4px;
+            width: fit-content;
+        }
+
+        .medsos-item {
+            font-size: 16px
+        }
+    }
+</style>
